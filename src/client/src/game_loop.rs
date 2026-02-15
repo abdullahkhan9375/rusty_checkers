a4e9a7597e4b3a6f6a329bf2639372294e2b7832
Abdullah Khan <abdullahkhan16021999@gmail.com>
2026-02-15 16:30:54 +1100

    using split_once

--- src/client/src/game_loop.rs
+++ src/client/src/game_loop.rs
@@ -3,26 +3,22 @@ use tokio::sync::mpsc;
 
 pub fn run(tx: mpsc::Sender<ClientMessage>, mut rx: mpsc::Receiver<ServerMessage>) {
     loop {
-        println!("Please type a message in <user>@<message> format: ");
+        println!("\nPlease type a message in <user>@<message> format: ");
         let mut message = String::new();
         let _ = std::io::stdin().read_line(&mut message);
         message.truncate(message.len() - 1); // trim newline
         if !message.contains('@') {
             eprint!("invalid message format, please use <recipient>@<message>");
         }
-        let v: Vec<&str> = message.split('@').collect();
-        if v.len() < 2 {
-            eprint!("message is missing either a recipient or content")
-        }
-
-        let recipient = v[0].to_string();
-        let content = v[1].to_string();
-
-        if let Err(e) = tx.blocking_send(ClientMessage::SendChatMessage {
-            recipient,
-            message: content,
-        }) {
-            eprintln!("GameLoop: Failed to post chat message: {e}");
+        if let Some((recipient, content)) = message.split_once('@') {
+            if let Err(e) = tx.blocking_send(ClientMessage::SendChatMessage {
+                recipient: recipient.to_string(),
+                message: content.to_string(),
+            }) {
+                eprintln!("GameLoop: Failed to post chat message: {e}");
+            }
+        } else {
+            eprint!("\nmessage is missing either a recipient or content")
         }
 
         if let Ok(ServerMessage::RecvChatMessage { from, message }) = rx.try_recv() {
